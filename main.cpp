e64bd96a996a53060561740c1abc0e42499cab91
MAU143429 <mau141226@estudiantec.cr>
2021-03-02 14:57:06 -0600

    update in main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "Node/Node.h"
+#include "SImply LInked List/SimplyLL.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SLL *list = new SLL();
+
+    list->append(1);
+    list->append(2);
+    list->append(3);
+
+    list->show();
+
     return 0;
 }
+
+
